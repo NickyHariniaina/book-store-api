75736f860b6807673b9d3e85d99766cce4d014b9
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-03 10:24:46 +0300

    feat: add book edition entity

--- src/main/java/com/onlydevs/bookstore/model/BookEdition.java
+++ src/main/java/com/onlydevs/bookstore/model/BookEdition.java
@@ -1,8 +1,12 @@
 package com.onlydevs.bookstore.model;
 
+import com.onlydevs.bookstore.model.enums.BookFormat;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.Instant;
 import java.util.UUID;
 
 @Entity
@@ -18,7 +22,23 @@ public class BookEdition {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(name= "\"isbn\"",unique = true,nullable = false)
+    @Column(unique = true,nullable = false)
     private String isbn;
 
+    private Publisher publisher;
+
+    private String edition;
+
+    @Enumerated(EnumType.STRING)
+    private BookFormat format;
+
+    private Boolean active = true;
+
+    @CreationTimestamp
+    @Column(name = "\"created_at\"")
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "\"updated_at\"")
+    private Instant updatedAt;
 }
