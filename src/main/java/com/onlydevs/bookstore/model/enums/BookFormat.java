ef5957e8396b9a10788bf9b11c732a73a8508b8c
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-03 09:39:44 +0300

    feat: add enums

---
+++ src/main/java/com/onlydevs/bookstore/model/enums/BookFormat.java
@@ -0,0 +1,8 @@
+package com.onlydevs.bookstore.model.enums;
+
+public enum BookFormat {
+    HARDCOVER,
+    PAPERBACK,
+    EBOOK,
+    AUDIOBOOK
+}
---
+++ src/main/java/com/onlydevs/bookstore/model/enums/PaymentMethod.java
@@ -0,0 +1,8 @@
+package com.onlydevs.bookstore.model.enums;
+
+public enum PaymentMethod {
+    CASH,
+    CARD,
+    MOBILE_MONEY,
+    BANK_TRANSFER
+}
