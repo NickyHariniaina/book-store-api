a120b7951e1dd23f3f232e3a3e50f8bcc7da3ed1
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-03 10:37:05 +0300

    feat: add book entity

---
+++ src/main/java/com/onlydevs/bookstore/model/Book.java
@@ -0,0 +1,40 @@
+package com.onlydevs.bookstore.model;
+
+import jakarta.persistence.*;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "\"book\"")
+public class Book {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(nullable = false)
+  private String title;
+
+  private String summary;
+
+  private String language;
+
+  @Column(name = "\"cover_url\"")
+  private String coverUrl;
+
+  @CreationTimestamp
+  @Column(name = "\"created_at\"")
+  private Instant createdAt;
+
+  @UpdateTimestamp
+  @Column(name = "\"updated_at\"")
+  private Instant updatedAt;
+}
