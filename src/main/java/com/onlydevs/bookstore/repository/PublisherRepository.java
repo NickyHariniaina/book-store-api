b76d50071dc2f3bc3684b35bf4721007b5183efc
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-09 17:53:16 +0300

    feat: add email uniqueness and search by name methods to PublisherRepository

--- src/main/java/com/onlydevs/bookstore/repository/PublisherRepository.java
+++ src/main/java/com/onlydevs/bookstore/repository/PublisherRepository.java
@@ -2,6 +2,12 @@ package com.onlydevs.bookstore.repository;
 
 import com.onlydevs.bookstore.model.Publisher;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PublisherRepository extends JpaRepository<Publisher, UUID> {}
+public interface PublisherRepository extends JpaRepository<Publisher, UUID> {
+  boolean existsByEmailIgnoreCase(String email);
+
+  Page<Publisher> findByNameContainingIgnoreCase(String name, Pageable pageable);
+}
