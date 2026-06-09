9bde698ab0f550c873cd436235c2c9592cfbdc11
NickyHariniai <ferrissushi@gmail.com>
2026-06-09 18:44:11 +0300

    fix: use authormapper in the service

--- src/main/java/com/onlydevs/bookstore/service/AuthorService.java
+++ src/main/java/com/onlydevs/bookstore/service/AuthorService.java
@@ -1,7 +1,8 @@
 package com.onlydevs.bookstore.service;
 
-import com.onlydevs.bookstore.model.Author;
+import com.onlydevs.bookstore.endpoint.rest.mapper.AuthorMapper;
 import com.onlydevs.bookstore.model.dto.CreateAuthorRequest;
+import com.onlydevs.bookstore.model.dto.CreateAuthorResponse;
 import com.onlydevs.bookstore.repository.AuthorRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,10 +10,12 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class AuthorService {
-    public final AuthorRepository authorRepository;
+  public final AuthorRepository authorRepository;
+  public final AuthorMapper authorMapper;
 
-    public CreateAuthorResponse save(CreateAuthorRequest request) {
-        var authorToCreate = request.toAuthor();
-        return authorRepository.save(authorToCreate);
-    }
+  public CreateAuthorResponse save(CreateAuthorRequest request) {
+    var authorToCreate = authorMapper.toDomain(request);
+    var authorCreated = authorRepository.save(authorToCreate);
+    return authorMapper.toRest(authorCreated);
+  }
 }
