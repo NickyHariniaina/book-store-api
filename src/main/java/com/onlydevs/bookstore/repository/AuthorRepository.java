6395ab429cccdbdf253d9239cf10cf5046bd8120
NickyHariniai <ferrissushi@gmail.com>
2026-06-09 18:40:55 +0300

    chore: add author repository

--- src/main/java/com/onlydevs/bookstore/repository/AuthorRepository.java
+++ src/main/java/com/onlydevs/bookstore/repository/AuthorRepository.java
@@ -1,11 +1,9 @@
 package com.onlydevs.bookstore.repository;
 
 import com.onlydevs.bookstore.model.Author;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface AuthorRepository extends JpaRepository<Author, UUID> {
-}
+public interface AuthorRepository extends JpaRepository<Author, UUID> {}
