47fefc1abec3d7bd1da0b206b63f5f7ed157838a
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-09 16:11:33 +0300

    feat: implement request genre dto

---
+++ src/main/java/com/onlydevs/bookstore/model/dto/CreateGenreRequest.java
@@ -0,0 +1,21 @@
+package com.onlydevs.bookstore.model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+class CreateGenreRequest {
+  @NotBlank(message = "Name is required")
+  @Size(max = 100)
+  public String name;
+
+  @Size(max = 500)
+  public String description;
+}
---
+++ src/main/java/com/onlydevs/bookstore/model/dto/RenameGenreRequest.java
@@ -0,0 +1,18 @@
+package com.onlydevs.bookstore.model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+class RenameGenreRequest {
+  @NotBlank(message = "Name is required")
+  @Size(max = 100)
+  public String name;
+}
