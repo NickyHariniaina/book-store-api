d0985ab633e6bbf07d80e792841e0dcab370a110
NickyHariniai <ferrissushi@gmail.com>
2026-06-10 11:32:03 +0300

    chore: initial test file for service

---
+++ src/test/java/com/onlydevs/bookstore/service/AuthorServiceTest.java
@@ -0,0 +1,12 @@
+package com.onlydevs.bookstore.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorServiceTest {
+
+    @Test
+    void save() {
+    }
+}
