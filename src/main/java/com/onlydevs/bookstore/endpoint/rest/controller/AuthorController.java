ea03d16d852a225fa81d6a779996580d2c6e3631
NickyHariniai <ferrissushi@gmail.com>
2026-06-11 08:55:40 +0300

    chore: add findById controller

--- src/main/java/com/onlydevs/bookstore/endpoint/rest/controller/AuthorController.java
+++ src/main/java/com/onlydevs/bookstore/endpoint/rest/controller/AuthorController.java
@@ -5,6 +5,8 @@ import com.onlydevs.bookstore.model.dto.CreateAuthorRequest;
 import com.onlydevs.bookstore.service.AuthorService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +21,10 @@ public class AuthorController {
   @PostMapping
   public AuthorResponse createAuthor(@RequestBody @Valid CreateAuthorRequest request) {
     return authorService.save(request);
+  }
+
+  @GetMapping("/{id}")
+  public AuthorResponse findById(@PathVariable String id) {
+    return authorService.findById(id);
   }
 }
