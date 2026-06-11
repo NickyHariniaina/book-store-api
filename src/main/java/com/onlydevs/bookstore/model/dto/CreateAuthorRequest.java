c25d700bace6393719545eb59e3a7e4313d9e876
NickyHariniai <ferrissushi@gmail.com>
2026-06-11 11:23:59 +0300

    fix: add string limitation to attribute in the create author req

--- src/main/java/com/onlydevs/bookstore/model/dto/CreateAuthorRequest.java
+++ src/main/java/com/onlydevs/bookstore/model/dto/CreateAuthorRequest.java
@@ -1,5 +1,10 @@
 package com.onlydevs.bookstore.model.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record CreateAuthorRequest(@NotBlank String firstName, @NotBlank String lastName) {}
+public record CreateAuthorRequest(
+    @NotBlank @Size(min = 1, max = 100, message = "first name must be between 1 and 100 characters")
+        String firstName,
+    @NotBlank @Size(min = 1, max = 100, message = "last name must be between 1 and 100 characters")
+        String lastName) {}
