3bb0bdd118ab09733165be3cb2074054cba0d555
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-18 17:29:35 +0300

    feat: add hand-written Genre DTOs

---
+++ src/main/java/com/onlydevs/bookstore/model/dto/request/RenameGenreRequest.java
@@ -0,0 +1,16 @@
+package com.onlydevs.bookstore.model.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RenameGenreRequest {
+  private String name;
+}
