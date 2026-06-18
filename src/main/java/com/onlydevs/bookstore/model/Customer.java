9aa3e1aa5291c0d1f38bc1575f96b10d389e2f2b
NickyHariniai <ferrissushi@gmail.com>
2026-06-18 16:58:50 +0300

    feat: add updatedAt timestamp to Customer entity
    Add @UpdateTimestamp field and Flyway migration V56 to add the column.

--- src/main/java/com/onlydevs/bookstore/model/Customer.java
+++ src/main/java/com/onlydevs/bookstore/model/Customer.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @Table(name = "\"customer\"")
@@ -50,6 +51,10 @@ public class Customer {
   @CreationTimestamp
   @Column(name = "\"created_at\"")
   private Instant createdAt;
+
+  @UpdateTimestamp
+  @Column(name = "\"updated_at\"")
+  private Instant updatedAt;
 
   @Builder.Default
   @OneToMany(mappedBy = "customer")
