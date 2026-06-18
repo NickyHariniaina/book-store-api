df48a1a68f586f60b3cb193feb342f4e90f0f232
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-18 17:49:30 +0300

    fix: precede endpoints with api v1

--- src/main/java/com/onlydevs/bookstore/endpoint/rest/controller/GenreController.java
+++ src/main/java/com/onlydevs/bookstore/endpoint/rest/controller/GenreController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/genres")
+@RequestMapping("/api/v1/genres")
 @RequiredArgsConstructor
 public class GenreController {
 
