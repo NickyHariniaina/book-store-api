8b55c138fc246ee67f5bba5926f14971bbacc4d0
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-18 18:08:52 +0300

    feat: add hand-written Publisher DTOs

---
+++ src/main/java/com/onlydevs/bookstore/model/dto/request/UpdatePublisherRequest.java
@@ -0,0 +1,20 @@
+package com.onlydevs.bookstore.model.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdatePublisherRequest {
+  private String name;
+  private String website;
+  private String email;
+  private String phone;
+  private String country;
+}
