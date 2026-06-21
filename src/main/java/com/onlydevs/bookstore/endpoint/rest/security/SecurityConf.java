44032c4584b0bd2daaa5c9c5ab226e2e3184875b
NickyHariniaina <ferrissushi@gmail.com>
2026-06-21 15:03:01 +0300

    chore: add secu settings

---
+++ src/main/java/com/onlydevs/bookstore/endpoint/rest/security/SecurityConf.java
@@ -0,0 +1,36 @@
+package com.onlydevs.bookstore.endpoint.rest.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConf {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http
+        .csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(s -> s.sessionCreationPolicy(STATELESS))
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(GET, "/ping", "/health/**").permitAll()
+            .requestMatchers(GET, "/api/v1/books/**", "/api/v1/authors").permitAll()
+            .requestMatchers("/api/v1/**").hasRole("ADMIN")
+            .anyRequest().authenticated()
+        ).build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
