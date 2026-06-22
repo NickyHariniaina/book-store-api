d5ea67a5608d803a30ef6feb57cfb7bb612283d9
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-22 08:42:41 +0300

    test: add @AutoConfigureMockMvc(addFilters=false) to PublisherControllerTest

--- src/test/java/com/onlydevs/bookstore/endpoint/rest/controller/PublisherControllerTest.java
+++ src/test/java/com/onlydevs/bookstore/endpoint/rest/controller/PublisherControllerTest.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
@@ -25,6 +26,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(PublisherController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class PublisherControllerTest {
 
   @MockBean private PublisherService publisherService;
