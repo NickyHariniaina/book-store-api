384a7f9ee32b422fe4781cf5ed1e170740b0bca7
NickyHariniaina <ferrissushi@gmail.com>
2026-07-09 20:23:13 +0300

    fix: redo the var on the save

--- src/main/java/com/onlydevs/bookstore/service/SaleService.java
+++ src/main/java/com/onlydevs/bookstore/service/SaleService.java
@@ -54,7 +54,7 @@ public class SaleService {
     var sale =
         Sale.builder().bookStore(store).customer(customer).status(SaleStatus.PENDING).build();
 
-    Sale saved = saleRepository.save(sale);
+    var saved = saleRepository.save(sale);
     return saleMapper.toRest(saved);
   }
 
