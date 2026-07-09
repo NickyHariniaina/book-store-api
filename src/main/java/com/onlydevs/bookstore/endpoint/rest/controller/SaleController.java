63396c82649924ba28b998ec7773281a18ceedff
Tafita Mathieu <199898191+Mathieu-bot@users.noreply.github.com>
2026-07-09 18:58:27 +0300

    fix: add missing RequestMapping annotation to SaleController

--- src/main/java/com/onlydevs/bookstore/endpoint/rest/controller/SaleController.java
+++ src/main/java/com/onlydevs/bookstore/endpoint/rest/controller/SaleController.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 @RequiredArgsConstructor
 public class SaleController {
 
