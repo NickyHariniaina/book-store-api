b58c8458c337f66218f70ced6bbf3e350041a0c2
colombefioren <colomberakotonjanahary@gmail.com>
2026-07-09 20:46:22 +0300

    refactor: remove store-filtering methods from repositories

--- src/main/java/com/onlydevs/bookstore/repository/InventoryMovementRepository.java
+++ src/main/java/com/onlydevs/bookstore/repository/InventoryMovementRepository.java
@@ -1,7 +1,6 @@
 package com.onlydevs.bookstore.repository;
 
 import com.onlydevs.bookstore.model.InventoryMovement;
-import com.onlydevs.bookstore.model.enums.InventoryMovementType;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -11,13 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface InventoryMovementRepository extends JpaRepository<InventoryMovement, UUID> {
 
-  @EntityGraph(attributePaths = {"bookEdition.book", "bookStore"})
-  List<InventoryMovement> findByBookStoreId(UUID storeId);
-
-  @EntityGraph(attributePaths = {"bookEdition.book", "bookStore"})
-  List<InventoryMovement> findByBookStoreIdAndInventoryMovementType(
-      UUID storeId, InventoryMovementType type);
-
-  @EntityGraph(attributePaths = {"bookEdition.book", "bookStore"})
+  @EntityGraph(attributePaths = {"bookEdition.book"})
   List<InventoryMovement> findByBookEditionId(UUID editionId);
 }
