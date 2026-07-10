db8d336a7e79d4640703d09555d69329608096c0
colombefioren <colomberakotonjanahary@gmail.com>
2026-07-10 07:11:28 +0300

    refactor: remove stock endpoints from BookController

--- src/main/java/com/onlydevs/bookstore/endpoint/rest/controller/BookController.java
+++ src/main/java/com/onlydevs/bookstore/endpoint/rest/controller/BookController.java
@@ -1,15 +1,12 @@
 package com.onlydevs.bookstore.endpoint.rest.controller;
 
-import com.onlydevs.bookstore.endpoint.rest.mapper.InventoryMapper;
 import com.onlydevs.bookstore.model.dto.request.CreateBookRequest;
 import com.onlydevs.bookstore.model.dto.request.UpdateBookRequest;
 import com.onlydevs.bookstore.model.dto.response.BookAuthorResponse;
 import com.onlydevs.bookstore.model.dto.response.BookDetailResponse;
 import com.onlydevs.bookstore.model.dto.response.BookSummaryResponse;
-import com.onlydevs.bookstore.model.dto.response.InventoryItemResponse;
 import com.onlydevs.bookstore.service.BookService;
 import jakarta.validation.Valid;
-import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -34,7 +31,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class BookController {
   private final BookService bookService;
-  private final InventoryMapper inventoryMapper;
 
   @GetMapping
   public ResponseEntity<Page<BookSummaryResponse>> getAllBooks(
@@ -96,16 +92,5 @@ public class BookController {
       @PathVariable UUID id, @PathVariable UUID genreId) {
     bookService.removeGenreFromBook(id, genreId);
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-  }
-
-  @GetMapping("/{bookId}/stock")
-  public ResponseEntity<Integer> getBookTotalStock(@PathVariable UUID bookId) {
-    return ResponseEntity.ok(bookService.getBookTotalStock(bookId));
-  }
-
-  @GetMapping("/low-stock")
-  public ResponseEntity<List<InventoryItemResponse>> getAllLowStock() {
-    var items = bookService.getAllLowStock();
-    return ResponseEntity.ok(inventoryMapper.toRestList(items));
   }
 }
